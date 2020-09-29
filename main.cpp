76e8eb8801287dc093495d7a6223b53090a1cfe0
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-09-29 14:17:02 -0500

    prueba01

--- main.cpp
+++ main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <conio.h>
+#include <string.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    char caracter;
+    int entero;
+    short corto;
+    float racional;
+    double doble;
+    long largo;
+
+    getline(cin, cadena);
+    if(cadena == "char"){
+        cout << sizeof(char) << endl;
+    }
+    fflush(stdin);
+
+    getline(cin, cadena);
+    if(cadena == "int"){
+        cout << sizeof(int) << endl;
+    }
+    fflush(stdin);
+
+    getline(cin, cadena);
+    if(cadena == "corto"){
+        cout << sizeof(short) << endl;
+    }
+    fflush(stdin);
+
+    getline(cin, cadena);
+    if(cadena == "racional"){
+        cout << sizeof(float) << endl;
+    }
+    fflush(stdin);
+
+    getline(cin, cadena);
+    if(cadena == "doble"){
+        cout << sizeof(double) << endl;
+    }
+    fflush(stdin);
+
+    getline(cin, cadena);
+    if(cadena == "largo"){
+        cout << sizeof(long) << endl;
+    }
+
 
     return 0;
 }
